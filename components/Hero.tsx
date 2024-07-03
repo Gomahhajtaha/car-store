be668881971c210462a851d1fb430e5d7a923b86
Gomah Haj taha <gomahhajtaha@gomahs-laptop.speedport.ip>
2024-07-04 00:30:43 +0200

    adding CustomButton file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Search, book, or rent a car - quickly and easily!
+
+            </h1>
+            <p className="hero__subtitle">
+
+                Streamline your car rental experience with our effortless process,
+
+            </p>
+            <CustomButton/>
+        </div>
+    </div>
   )
 }
 
