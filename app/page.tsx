d08f55ac2874069cb25f673c7b5e838dc228725b
Gomah Haj taha <gomahhajtaha@gomahs-laptop.speedport.ip>
2024-07-03 23:41:01 +0200

    install next js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" overflow-hidden flex min-h-screen flex-col items-center justify-between p-24">
+     
+    </main>
+  );
+}
