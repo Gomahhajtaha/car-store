9f16f6503eff76e142503b82c2bf9b31b3ddbedb
Gomah Haj taha <gomahhajtaha@gomahs-laptop.speedport.ip>
2024-07-04 17:56:16 +0200

    refactoring Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <head className="w-full absolute z-10">
+
+    <nav className="max-w-[1440px] mx-auto flex justify-between">
+
+    </nav>
+
+   </head>
   )
 }
 
-export default Navbar
+export default Navbar;
