b22c2c31a1806f88de0a4fab3b3899543d1085de
Gomah Haj taha <gomahhajtaha@gomahs-laptop.speedport.ip>
2024-07-07 09:57:56 +0200

    adding button and input

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,23 @@
 "use client";
-import { Combobox, ComboboxButton, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react';
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Transition } from '@headlessui/react'
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+import { manufacturers } from '@/constants';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps ) => {
+    const [query, setQuery] = useState('');
+
+    const filteredManufacturers = query === ""
+    ? manufacturers : manufacturers.filter((item) => (
+        item.toLocaleLowerCase()
+        .replace(/\s+/g, "")
+        .includes(query.toLowerCase().replace(/\s+/g,""
+            
+        ))
+    ))
+
+
   return (
     <div className='search-manufacturer'>
         <Combobox> 
@@ -22,6 +35,23 @@ const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerPr
             />
             
             </ComboboxButton>
+            <ComboboxInput className="search-manufacturer__input"
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string)=> manufacturer} 
+            onChange={(e) => setQuery(e.target.value)}/>
+
+            <Transition 
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100' 
+            leaveTo='opacity-0'
+            afterLeave={()=> setQuery('')} >
+
+                <ComboboxOptions>
+
+                </ComboboxOptions>
+
+            </Transition>
 
         </div>
         </Combobox>
