8d78584506eb1b6d4d2ce98692bde15557368912
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-10 23:54:48 +0200

    adding imgs to the view more btn in CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -65,6 +65,21 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
                   <Image src= "/hero.png"  alt='car model' fill priority className='object-contain' /> 
                     
                      </div>
+                     <div className='flex gap-3'>
+                     <div className='flex gap-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+
+                     <Image src= "/hero.png"  alt='car model' fill priority className='object-contain' /> 
+
+
+                     </div>
+                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                      <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain' />
+                    </div>
+                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                      <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
+                    </div>
+
+                     </div>
                   </div>
 
             </DialogPanel>
