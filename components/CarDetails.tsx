f67dc0eb8bce64be25c63937dbdb02590eb1623a
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-10 23:45:26 +0200

    refactoring the view more btn

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -44,13 +44,15 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
           leaveTo='opacity-0 scale-95'
 
         >
-            <DialogPanel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left'>
+            <DialogPanel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
 
                 <button type="button" 
+                className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+
                 onClick={closeModal}>
 
                     <Image
-                    src="close.svg"
+                    src="/close.svg"
                     alt="close"
                     width={20}
                     height={20}
@@ -58,6 +60,12 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
                     />
 
                 </button>
+                <div className='flex-1 flex flex-col gap-3'>
+                  <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                  <Image src= "/hero.png"  alt='car model' fill priority className='object-contain' /> 
+                    
+                     </div>
+                  </div>
 
             </DialogPanel>
              </TransitionChild>
