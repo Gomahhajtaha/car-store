32d6524a81745b46b97d229d0f2abf9b3c58600c
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-12 17:18:53 +0200

    adding searchbtn to searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,20 @@
 
 import { useState } from 'react';
 import SearchManufacturer from './SearchManufacturer';
+import Image from 'next/image';
+
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
 
 const SearchBar = () => {
    const [manufacturer, setManufacturer] = useState("");
@@ -15,8 +29,11 @@ const SearchBar = () => {
     <SearchManufacturer 
     manufacturer={manufacturer} 
     setManufacturer={setManufacturer}/>
+    <SearchButton otherClasses='sm:hidden' />
 
 </div>
+
+
   </form>
   )
 }
