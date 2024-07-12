2a35053beb5a9b24c49f5e259a547ec0edbd8038
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-12 17:02:06 +0200

    adding angle to cardata

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -68,13 +68,13 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
                      <div className='flex gap-3'>
                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
 
-                     <Image src= {generateCarImageUrl(car)}  alt='car model' fill priority className='object-contain' /> 
+                     <Image src= {generateCarImageUrl(car,'29')}  alt='car model' fill priority className='object-contain' /> 
                      </div>
                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                      <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car,'33')} alt='car model' fill priority className='object-contain' />
                     </div>
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                      <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car,'14')} alt='car model' fill priority className='object-contain' />
                     </div>
                      </div>
                      </div>
