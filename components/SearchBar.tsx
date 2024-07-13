734e860fa8b641fd783bcc94004f2dcaef26dfbc
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-13 08:16:50 +0200

    adding Searc Icon to the searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,6 +19,7 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
 const SearchBar = () => {
    const [manufacturer, setManufacturer] = useState("");
+   const [model, setModel] = useState("");
 
     const handleSearch = () => {}
   return (
@@ -32,6 +33,26 @@ const SearchBar = () => {
     <SearchButton otherClasses='sm:hidden' />
 
 </div>
+
+<div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
 
 
   </form>
