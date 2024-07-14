bee1e49a973f75cced9c2252443739f8c75df2f7
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-14 23:22:52 +0200

    adding ListOption to CustomFilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,7 +4,7 @@
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 
 const CustomFilter = ({title, options}: CustomFilterProps) => {
@@ -18,24 +18,45 @@ const [selected, setSelected] = useState(options[0]);
       onChange={(e)=> setSelected(e)}>
         <div className="relative w-fit z-10">
            {/* Button for the listbox */}
-           <Listbox.Button className='custom-filter__btn'>
+           <ListboxButton className='custom-filter__btn'>
 
             <span className="block truncate">
            {selected.title}
             </span>
 
             <Image
-            src="chevron-up-down.svg"
+            src="/chevron-up-down.svg"
             width={20}
             height={20}
             className="ml-4 object-contain"
             alt="chevron up down"
             />
 
+        </ListboxButton>
 
+      <Transition
+      as={Fragment}
+      leave="transition ease-in duration-100 "
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+      >
 
+        <ListboxOptions className="custom-filter__options">
+          {options.map((option) => (
+            <ListboxOption> 
 
-        </Listbox.Button>
+              {({selected}) => (
+                <span>
+                  {option.title}
+                </span>
+              )}
+
+            </ListboxOption>
+          ))}
+
+        </ListboxOptions>
+        
+        </Transition>  
         </div>
 
       </Listbox>
