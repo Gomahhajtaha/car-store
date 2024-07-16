8518cde5b27ea2e20f001f8d905c871fd00f9c08
Gomah Haj taha <jummah-haj-taha@hotmail.com>
2024-07-16 23:38:27 +0200

    importing the CustomButton

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,13 +1,22 @@
 'use client';
 import { ShowMoreProps } from "@/types";
 import { useRouter } from "next/navigation";
+import CustomButton from "./CustomButton";
 
 const ShowMore = ({pageNumber, isNext}:ShowMoreProps) => {
 
   const router = useRouter();
   const handleNavigation = () =>{}
   return (
-    <div>ShowMore</div>
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton title="Show More"
+        btnType="button"
+        containerStyles="bg-primary-blue rounded-full text-white
+        "/>
+      )}
+    
+    </div>
   )
 }
 
